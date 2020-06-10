28a5e630c7f6ce003d7199a1283555c51846ec0f
kiran ram <kram@lions.molloy.edu>
2020-06-10 02:37:54 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+  
+  attr_reader :name
+  
+  
+  def initialize(name)
+    @balance = 100 
+    @status = "open"
+  end
 
+ def deposit(deposit)
+   @balance += deposit 
+ end
+ 
+ def display_balance
+   @balance 
+ end
+ 
+ def valid?
+   if @balance = 100 && @status = "open"
+ end
+ 
+ def close_account
+   @balance = 0 
+   @status = "closed"
+ end
+  
 end

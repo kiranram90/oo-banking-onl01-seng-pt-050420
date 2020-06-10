d979dbca27b952a88ab330b6e44baad5eadd3c4b
kiran ram <kram@lions.molloy.edu>
2020-06-10 02:49:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,13 +3,19 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :transfer_amount
   
   
-  def initialize(sender, receiver)
-    @name = name
-    @tra = 1000 
+  def initialize(sender, receiver, transfer_amount)
+    @sender =  sender 
+    @receiver = receiver 
+    @transfer_amount = transfer_amount
     @status = "pending"
   end
 
  def valid? 
+   if sender.valid? = true && receiver.valid? = true 
+     true 
+   else 
+     false 
+   end
    
  end
  

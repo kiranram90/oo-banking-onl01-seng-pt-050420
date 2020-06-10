5854ae2c7623f2e843d28c8e44788b5fc9111d53
kiran ram <kram@lions.molloy.edu>
2020-06-10 03:42:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,17 @@ class Transfer
  end
  
  def execute_transaction(sender:, receiver:, amount:)
-   Transfer.new(sender, receiver, amount)
-   end
+   if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
  end
    
  
